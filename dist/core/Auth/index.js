2aaf1d5565146da2276ea97363fc0e30f5d1cbd0
RixzzOffc <fattahxml12@gmail.com>
2026-02-15 08:00:05 +0700

    Upload dist/core/Auth/index.js

---
+++ dist/core/Auth/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+  return mod && mod.__esModule ? mod : { "default": mod };
+};
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.useFolderAuth = void 0;
+
+const folder_1 = __importDefault(require("./folder"));
+exports.useFolderAuth = folder_1.default;
+
+exports.default = {
+  useFolderAuth: folder_1.default,
+};
